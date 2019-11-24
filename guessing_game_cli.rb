109162a0d36a4386c28b8f640926919f8b7bec8c
Amanda Rolon <abrolon87@gmail.com>
2019-11-24 17:42:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,8 +6,14 @@ def get_user_input
   input = gets.chomp
 end
 
+run_guessing_game
+
+get_user_input
+
 if get_user_input == "exit"
   puts "Goodbye!"
-elsif get_user_input != special
-  puts ("Sorry! The computer guessed #{special}")
-  puts "You guessed the correct number!"
+  elsif get_user_input != special
+    puts ("Sorry! The computer guessed #{special}")
+  else
+    puts "You guessed the correct number!"
+end 
